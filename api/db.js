86f73f0bd0ea81bbd43b50680ffb73689bf6e8a8
Zoedube <rofhiwazoe11@gmail.com>
2025-03-26 19:43:08 +0200

    final commits

--- api/db.js
+++ api/db.js
@@ -4,8 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = mysql.createConnection({
-  host:"sql8.freesqldatabase.com",
-  user:"sql8765094",
-  password:"ayiMXBKeEY",
-  database:"sql8765094"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 })
